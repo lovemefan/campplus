e4db42bfe841522ff66c8b712bc0a0ab20d79186
lovemefan <lovemefan@outlook.com>
2023-06-26 13:21:05 +0800

    add onnx default path

--- campplus/src/Campplus.py
+++ campplus/src/Campplus.py
@@ -14,7 +14,7 @@ class Campplus:
         :param onnx_path: onnx model file path
         :param threshold: threshold of speaker embedding similarity
         """
-        self.onnx = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'onnx/campplus.onnx')
+        self.onnx = onnx_path or os.path.join(os.path.dirname(os.path.dirname(__file__)), 'onnx/campplus.onnx')
         self.sess = onnxruntime.InferenceSession(self.onnx)
         self.output_name = [nd.name for nd in self.sess.get_outputs()]
         self.threshhold = threshold
